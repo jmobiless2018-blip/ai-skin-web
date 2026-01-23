26f7ee87495e9d6301e8f184dc5506e7dee9d5c9
Thana <your-email@gmail.com>
2026-01-23 11:53:19 +0700

    final fix for render deployment

--- app.py
+++ app.py
@@ -2,24 +2,20 @@ from flask import Flask, render_template, request
 import os
 from model import predict_image
 
-# ✅ ต้องอยู่ก่อน route เสมอ
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "static/uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-
 @app.route("/")
 def index():
     return render_template("index.html")
-
 
 @app.route("/upload", methods=["POST"])
 def upload():
     file = request.files["image"]
     filename = file.filename
-
     filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
     file.save(filepath)
 
@@ -32,9 +28,7 @@ def upload():
         confidence=confidence,
         advice=advice
     )
-
-
-# ✅ ต้องมีเสมอ
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
