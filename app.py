1b1954409263fdb41d4055afb92971e7ac3368b8
Thana <your-email@gmail.com>
2026-01-23 17:50:38 +0700

    fix app for render

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    return "OK"
