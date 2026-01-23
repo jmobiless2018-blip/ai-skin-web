0789195c2d034662a3bef47666a89d079cbc3ada
Thana <your-email@gmail.com>
2026-01-23 12:13:34 +0700

    fix app.py for render deployment

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def upload():
         confidence=confidence,
         advice=advice
     )
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
-
